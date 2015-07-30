19a7a3bb1dfcae52b86d6f14fb9b6a6213c6a005
ilmlife <ilmlife@126.com>
2015-07-30 10:33:54 +0800

    spider for api

---
+++ src/com/ilmlife/net/spider/Spider.java
@@ -0,0 +1,79 @@
+package com.ilmlife.net.spider;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ *
+ * @author ilmlife E-Mail：ilmlife@126.com
+ * @version 1.0 创建时间：2015年7月24日 下午9:27:28
+ */
+public class Spider {
+	public static void main(String[] args) throws Exception {
+		String domain = "http://cn.cocos2d-x.org/";
+		String baseUrl = "http://cn.cocos2d-x.org/doc/jsdoc/index.html";
+		String localPath = "C:/Users/SillyBoy/Desktop/testSpider/";
+		
+		Spider spider = new Spider();
+		spider.spiderBegin(baseUrl, domain, localPath);
+	}
+	
+	/**
+	 * 从url获取数据并替换domain
+	 * @param url
+	 * @param domain
+	 * @param localPath
+	 * @throws Exception
+	 */
+	public void spiderBegin(String url,String domain,String localPath) throws Exception {
+		URL httpurl = new URL(url);
+		HttpURLConnection httpConn = (HttpURLConnection) httpurl.openConnection();
+		httpConn.setRequestProperty("User-Agent", "Opera/9.80 (Windows NT 6.1; WOW64) Presto/2.12.388 Version/12.16");
+		httpConn.setRequestMethod("GET");
+		httpConn.setDoInput(true);
+		
+		BufferedWriter fileBw = new BufferedWriter(new FileWriter(buildFile(url, domain, localPath)));
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(httpConn.getInputStream()));
+		String line;
+		while ((line = in.readLine()) != null) {
+			fileBw.write(line.replaceAll(domain, localPath));
+		}
+		fileBw.flush();
+		fileBw.close();
+		in.close();
+	}
+	
+	/**
+	 * 构建本地位置
+	 * @param url
+	 * @param domain
+	 * @param localPath
+	 * @return
+	 * @throws Exception
+	 */
+	public File buildFile(String url, String domain, String localPath) throws Exception {
+		String localFileStr = url.replaceAll(domain, localPath);
+		
+		File file = new File(localFileStr);
+		String pathStr = file.getParent();
+		File path = new File(pathStr);
+		if(!path.exists()) {
+			path.mkdirs();
+		}
+		if(!file.exists()) {
+			file.createNewFile();
+		}
+		
+		return file;
+	}
+	
+	
+}
